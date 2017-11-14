629771f9ffd53c40d87ac70b53a8e794e5634258
zhufengzhufeng <894918097@qq.com>
2017-11-14 10:05:52 +0800

    'todo'

--- week1-day3/todo.js
+++ week1-day3/todo.js
@@ -14,11 +14,18 @@ const vm = new Vue({
             {isSelected:false,title:'吃饭'}
         ],
         title:'',
-        cur:''
+        cur:'',
+        hash:''
     },
     created(){ // ajax获取 初始化数据
         // 如果localStorage中有数据 就用有的没数据 就用默认的
         this.todos = JSON.parse(localStorage.getItem('data')) || this.todos;
+        // 监控hash值的变化, 如果页面以及有hash了 重新刷新页面也要获取hash值
+        this.hash = window.location.hash.slice(2) || 'all';
+        window.addEventListener('hashchange',()=>{
+            // 当hash值变化 重新操作记录的数据
+            this.hash = window.location.hash.slice(2);
+        },false);
     },
     watch:{
         todos:{ // watch默认只监控一层的数据变化,深度监控
@@ -47,6 +54,12 @@ const vm = new Vue({
         }
     },
     computed:{
+        filterTodos(){
+            if(this.hash === 'all')  return this.todos;
+            if(this.hash === 'finish') return this.todos.filter(item=>item.isSelected);
+            if(this.hash === 'unfinish') return this.todos.filter(item=>!item.isSelected);
+            return this.todos;
+        },
         count(){
             return this.todos.filter(item=>!item.isSelected).length
         }
