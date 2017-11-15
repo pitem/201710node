304e4ed45bad0d49889616f5a8a1fbf80ee1b04f
zhufengzhufeng <894918097@qq.com>
2017-11-15 10:56:29 +0800

    'nextTick'

--- week2-day1/8.emit-on.js
+++ week2-day1/8.emit-on.js
@@ -24,5 +24,5 @@ let eat = (who) =>{console.log(who+'吃');};
 girl.on('失恋',cry); // {失恋:[cry]}
 girl.on('失恋',eat); // {失恋:[cry,eat]}
 girl.on('失恋',shopping); // {失恋:[cry,eat,shopping]}
-girl.emit('失恋','我','你');
+girl.emit('失恋');
 
