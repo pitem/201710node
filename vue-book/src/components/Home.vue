39f2817a7e9c3d9fe66178f76744b19ca3b1da88
zhufengzhufeng <894918097@qq.com>
2017-11-21 16:31:50 +0800

    add

---
+++ vue-book/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <MHeader>首页</MHeader>
+  </div>
+</template>
+<script>
+   //1.引入组件 2.注册组件 3.使用组件
+    import MHeader from '../base/MHeader.vue';
+    export default {
+        data(){
+            return {msg: 'hello'}
+        },
+        created(){
+        },
+        methods: {},
+        computed: {},
+        components: {
+          MHeader
+        }
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ vue-book/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router);
+import Add from '../components/Add.vue'
+import Home from '../components/Home.vue';
+import Collect from '../components/Collect.vue';
+import Detail from '../components/Detail.vue';
+import List from '../components/List.vue';
+export default new Router({
+  routes: [
+    {path:'/home',component:Home},
+    {path:'/collect',component:Collect},
+    {path:'/add',component:Add},
+    {path:'/detail',component:Detail},
+    {path:'/list',component:List},
+  ]
+})
