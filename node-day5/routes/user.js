ffb7c15cf4d6c75a6ec27aa108dfc15180faff59
zhufengzhufeng <894918097@qq.com>
2017-11-25 16:31:11 +0800

    'body-parser-middleware'

--- node-day5/routes/user.js
+++ node-day5/routes/user.js
@@ -7,7 +7,28 @@ router.get('/login',function (req,res) {
 });
 router.get('/reg',function (req,res) {
     // path.resolve是根据运行的文件的位置来解析的，所以此时不能用resolve
-    console.log(path.resolve('../views/reg.html'))
+   /* console.log(path.resolve('../views/reg.html'))*/
     res.sendFile(path.join(__dirname,'../views/reg.html'))
 });
+router.post('/reg',function (req,res) {
+    console.log(req.body);
+});
+router.post('/login',function (req,res) {
+
+});
 module.exports = router;
+
+/*
+ let str = '';
+ req.on('data',function (chunk) {
+ str+=chunk;
+ });
+ req.on('end',function () {
+ console.log(str); // username=admin&password=zhufeng
+ console.log(require('querystring').parse(str)); // node自带的模块
+ let obj ={}
+ str.replace(/([^&=]+)=([^&=]+)/g,function () {
+ obj[arguments[1]] = arguments[2]
+ });
+})
+* */
